6cb36372c03e409daeeaea282d1c564250845ce1
HafizAkinteye <55165896+HafizAkinteye@users.noreply.github.com>
2022-04-01 14:16:37 -0500

    Changes

---
+++ Collegion/urls.py
@@ -0,0 +1,38 @@
+"""Collegion URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from Collegion_Backend import views
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.contrib.auth.views import LogoutView
+from Collegion import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('logout/', LogoutView.as_view(next_page=settings.LOGOUT_REDIRECT_URL), name='logout'),
+    #path('logout/', logout,  name='logout'),
+    path('', views.index, name='index'),
+    path('register/', views.register_view, name='register'),
+    path('chat/', views.chat_view, name='chats'),
+     # URL form : "/api/messages/1/2"
+    path('api/messages/<int:sender>/<int:receiver>', views.message_list, name='message-detail'),  # For GET request.
+    # URL form : "/api/messages/"
+    path('api/messages/', views.message_list, name='message-list'),   # For POST
+    # URL form "/api/users/1"
+    path('api/users/<int:pk>', views.user_list, name='user-detail'),      # GET request for user with id
+    path('api/users/', views.user_list, name='user-list'),    # POST for new user and GET for all users list
+]
