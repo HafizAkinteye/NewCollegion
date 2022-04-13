48c13e2cc201c8d6a53d0515deee6fb6e8997094
caige13 <34533619+caige13@users.noreply.github.com>
2022-04-12 19:27:00 -0500

    Create chat_room app and made changes to models.

--- Collegion_Backend/models.py
+++ Collegion_Backend/models.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.models import User
 from django.db import models
+from chat_room import ChatRoom
+
 
 class Message(models.Model):
      sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
-     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver')
+     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver', null=True)
+     chat_room = models.ForeignKey(ChatRoom, on_delete=models.SET_NULL, related_name='chat_room', null=True)
      message = models.CharField(max_length=1200)
      timestamp = models.DateTimeField(auto_now_add=True)
      is_read = models.BooleanField(default=False)
@@ -11,3 +14,5 @@ class Message(models.Model):
            return self.message
      class Meta:
            ordering = ('timestamp',)
+
+
