b98538f1c607c989f8861cf88894352d8acc198b
caige13 <34533619+caige13@users.noreply.github.com>
2022-04-15 17:47:40 -0500

    fix urls

--- Collegion/urls.py
+++ Collegion/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url, include
+from django.conf.urls import include
 from Collegion_Backend import views
 from django.contrib.auth import login
 from django.contrib.auth import logout
