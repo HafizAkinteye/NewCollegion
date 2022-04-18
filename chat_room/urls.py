63797d5c06505a7eb2f94b2404e5bec8e2d9b18f
HafizAkinteye <55165896+HafizAkinteye@users.noreply.github.com>
2022-04-17 23:19:12 -0500

    Search stuff

--- chat_room/urls.py
+++ chat_room/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import create_chat_room, delete_chat_room, message_chat_view, invite_to_chat_room, \
-    remove_from_chat_room
-    
+    remove_from_chat_room, add_user_form, add_user
+
 app_name = 'chat_room'
 
 urlpatterns = [
     path('<int:chatroom_id>/', message_chat_view, name='message-chatrooms'),
     path('create/', create_chat_room, name='create-chatroom'),
+    path('addUserForm/<int:group_id>/', add_user_form, name = 'add-to-group' ),
+    path('addUser/<int:group_id>/<int:user_id>/', add_user, name = 'add-user' )
 ]
